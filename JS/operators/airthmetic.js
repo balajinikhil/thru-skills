028ca81fd119806ed3071cab3decca139c49f3e7
balajinikhil <balajiedward007@gmail.com>
2019-09-07 18:40:26 +0530

    07 sep

---
+++ JS/operators/airthmetic.js
@@ -0,0 +1,42 @@
+//arithmetic operators
+
+//1.Addition +
+//2.Substract -
+//3.Multiplication *
+//4.Division /
+//5.Modulus %
+//6.Increment ++
+//7.Decrement --
+
+var addition = 10 + 20;
+console.log(addition);
+
+var Substract = 10 - 20;
+console.log(Substract);
+
+var mul = 10 * 20;
+console.log(mul);
+
+var div = 10 / 20;
+console.log(div);
+
+var modulus = 277 % 10; //gives reminder
+console.log(modulus);
+
+//increment
+
+var x = 10;
+
+console.log("difference", ++x);
+x++; //post-increment
+console.log(x);
+
+var y = 10;
+++y; //pre-increment
+console.log(y);
+
+//decrement
+
+var a = 20;
+console.log(a--); //first it gives value then it's decremented
+console.log(--a); //decrementes first and then givesthe value
---
+++ JS/operators/assignment.js
@@ -0,0 +1,17 @@
+//Assigment operators
+
+//1.Assigment =
+//2.Addition Assigment +=
+//3.Substraction Assignment -=
+//4.Multiplication Assignment *=
+//5.Division Assignment /=
+//6.Modulus Assignment %=
+
+var x = 12;
+console.log("value of x", x);
+
+// x = x + 10;
+// console.log(x);
+
+x += 10;
+console.log(x);
