fec21b5acbacc496ed2c22a6091997b3b05ee8f1
balajinikhil <balajiedward007@gmail.com>
2019-09-22 14:51:03 +0530

    commit

---
+++ JS/conditions/switch-cases.js
@@ -0,0 +1,11 @@
+var lang = `Java`;
+switch (lang) {
+  case `JS`:
+    console.log(`java script`);
+    break;
+  case `python`:
+    console.log(`python`);
+    break;
+  default:
+    console.log(`choose a language `);
+}
