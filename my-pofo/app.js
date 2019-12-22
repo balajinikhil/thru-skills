da9408cdfd53027ae67f0b46d0c36531e094c793
balajinikhil <balajiedward007@gmail.com>
2019-12-22 12:48:14 +0530

    my-pofo

--- my-pofo/app.js
+++ my-pofo/app.js
@@ -1,38 +1,59 @@
-const express = require('express');
-const hbs = require('hbs');
-const middlewares = require('./middlewares/appMiddleware');
+const express = require("express");
+const hbs = require("hbs");
+const session = require("express-session");
+const middlewares = require("./middlewares/appMiddleware");
+const routes = require("./routes/index");
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.set('views', __dirname+'/views');
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname+'/views/partials')
+app.use(middlewares.logger);
 
+app.use(
+  session({
+    secret: "myappsupersecret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 10000000 }
+  })
+);
+app.use(middlewares.authenticated);
 
-app.use(middlewares.logger)
-app.use(express.static(__dirname+'/static'))
+app.use(express.static(__dirname + "/static"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/' , function(req,res) {
-    res.render('index')
-});
+app.get("/", routes.index);
+app.get("/projects", routes.projectList);
+app.get("/blogs", routes.blogList);
+app.get("/contact", routes.contact);
+app.post("/contact", routes.doContact);
+app.get("/admin", middlewares.authenticate, routes.admin);
 
+app.get("/signin", routes.signin);
 
-app.get('/projects', (req,res) => {
-    res.render('projectList')
-});
+app.post("/signin", routes.doSignin);
 
-app.get('/blogs', (req,res) => {
-    res.render('blog')
-});
+app.get("/signup", routes.signup);
 
+app.post("/signup", routes.doSignup);
+app.get("/signout", routes.signout);
 
-app.get('/project-detail', (req,res) => {
-    res.render('projectDetail')
-})
+app.get("/admin/projects", middlewares.authenticate, routes.adminProjectList);
+
+app.get("/project/:alias", routes.projectDetail);
+app.get("/blog/:alias", routes.blogDetail);
+app.get(
+  "/admin/project/:alias",
+  middlewares.authenticate,
+  routes.adminProjectDetail
+);
+
+app.get("/admin/project/new", middlewares.authenticate, routes.createNew);
 
 app.use(middlewares.notFound);
 app.use(middlewares.handleError);
 
-
-
-app.listen(3000, () => console.log('App up n running on port 3000'));
+app.listen(3000, () => console.log("App up n running on port 3000"));
--- my-pofo/middlewares/appMiddleware.js
+++ my-pofo/middlewares/appMiddleware.js
@@ -1,14 +1,30 @@
-module.exports.logger = (req,res,next) => {
-    console.log(req.method, req.url);
+module.exports.logger = (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+};
+
+module.exports.notFound = (req, res, next) => {
+  res.status(404).send("Not Found");
+};
+
+module.exports.handleError = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Something went wrong");
+};
+
+module.exports.authenticate = (req, res, next) => {
+  if (req.session.isLoggedIn) {
     next();
-}
+  } else {
+    res.redirect("/signin");
+  }
+};
 
-
-module.exports.notFound = (req,res,next) => {
-    res.status(404).send('Not Found');
-}
-
-module.exports.handleError = (err,req,res,next) => {
-    console.log(err);
-    res.status(500).send('Something went wrong');
-}
+module.exports.authenticated = (req, res, next) => {
+  if (req.session.isLoggedIn) {
+    res.locals.user = req.session.user;
+    next();
+  } else {
+    next();
+  }
+};
